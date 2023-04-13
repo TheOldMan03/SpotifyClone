694a344fd744b6f117e11bc8947577f338aea609
Joel Antony <joel.geoanto@gmail.com>
2023-04-13 21:19:38 +0530

    Made a button :)

--- spotifyclone/src/App.js
+++ spotifyclone/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi this is a test</h1>
+    <div className="app">
+
+      <Login/>
+  
     </div>
   );
 }
